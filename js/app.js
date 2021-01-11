78914cdd9ca7ed66675eb6ae2afbab795ed32fb9
alibhdrn <ali.bahadoran@yahoo.com>
2021-01-11 19:12:58 +0330

    validate length of fields

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 //variables
 const sendBtn = document.querySelector('#sendBtn')
+let email = document.querySelector('#email')
+let subject = document.querySelector('#subject')
+let message = document.querySelector('#message')
+
+
+
 
 
 
@@ -11,7 +17,10 @@ eventListeners()
 function eventListeners() {
     //app initialization
     document.addEventListener('DOMContentLoaded', appInit)
-
+    //validating fields
+    email.addEventListener('blur', validateField)
+    subject.addEventListener('blur', validateField)
+    message.addEventListener('blur', validateField)
 }
 
 
@@ -20,5 +29,21 @@ function eventListeners() {
 
 //functions
 function appInit() {
+    //disabled send button on load
     sendBtn.disabled = true
+}
+
+//validating field of form
+function validateField() {
+    validateLength(this)
+}
+//validate length of fields
+function validateLength(field) {
+    if (field.value.length > 0) {
+        field.style.borderBottomColor = 'green'
+        field.classList.remove('error')
+    } else {
+        field.style.borderBottomColor = 'red'
+        field.classList.add('error')
+    }
 }
